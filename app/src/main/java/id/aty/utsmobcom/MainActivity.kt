1f9bea006323f75e80ba65b57f69af2496848520
atylestari <atylestari98@gmail.com>
2020-10-28 15:46:16 +0700

    first commit

--- app/src/main/java/id/aty/utsmobcom/MainActivity.kt
+++ app/src/main/java/id/aty/utsmobcom/MainActivity.kt
@@ -3,13 +3,19 @@ package id.aty.utsmobcom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private var score = 0
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_plus2 = findViewById(R.id.btn_plus) as Button
+        btn_plus2.setOnClickListener {
+            Toast.makeText(this, "Plus", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
